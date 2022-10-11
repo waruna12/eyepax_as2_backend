535c30abe81f8bbb948a6a21fe4965c05de187ca
waruna12 <waruna532@gmail.com>
2022-10-11 09:08:54 +0530

    remove comments

--- src/route/index.js
+++ src/route/index.js
@@ -15,29 +15,4 @@ router.post("/login", async function (req, res) {
   await signupController.login(req, res);
 });
 
-// router.get("/user/search/:key", async function (req, res) {
-//   await signupController.search(req, res);
-// });
-
-// router.get("/find-user/:email", async function (req, res) {
-//   await signupController.findUser(req, res);
-// });
-
-// router.get("/user/:id", async function (req, res) {
-//   await signupController.getID(req, res);
-// });
-
-// router.get("/user", async function (req, res) {
-//   await signupController.getall(req, res);
-// });
-
-// router.put("/user/:id/:newPassword", async function (req, res) {
-//   await signupController.update(req, res);
-// });
-
-// router.put("/user/:id", async function (req, res) {
-//   await signupController.profileUpdate(req, res);
-// });
-
-//use anyware export
 module.exports = router;
